4956bf8d632b91586f15211aa8f19801ac2f4401
Adam Young <me@adam-young.co.uk>
2026-06-07 02:06:28 +0100

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.swift
@@ -0,0 +1,23 @@
+class Solution {
+    func maxArea(_ heights: [Int]) -> Int {
+        guard heights.count >= 2 else {
+            return 0
+        }
+
+        var left = 0
+        var right = heights.count - 1
+
+        var maxWater = 0
+        while left < right {
+            let water = min(heights[left], heights[right]) * (right - left)
+            maxWater = max(maxWater, water)
+            if heights[left] < heights[right] {
+                left += 1
+            } else {
+                right -= 1
+            }
+        }
+
+        return maxWater
+    }
+}
