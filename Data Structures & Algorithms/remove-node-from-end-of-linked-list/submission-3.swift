10c1d9bb4d429428cc157dee7cd882a5305bb844
Adam Young <me@adam-young.co.uk>
2026-06-07 14:56:22 +0100

    Add: remove-node-from-end-of-linked-list - submission-3

---
+++ Data Structures & Algorithms/remove-node-from-end-of-linked-list/submission-3.swift
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     var val: Int
+ *     var next: ListNode?
+ *     init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func removeNthFromEnd(_ head: ListNode?, _ n: Int) -> ListNode? {
+        let dummy = ListNode(0)
+        dummy.next = head
+        var slow: ListNode? = dummy
+        var fast: ListNode? = dummy
+
+        for _ in 0..<n {
+            fast = fast?.next
+        }
+
+        while fast?.next != nil {
+            slow = slow?.next
+            fast = fast?.next
+        }
+
+        slow?.next = slow?.next?.next
+
+        return dummy.next
+    }
+}
